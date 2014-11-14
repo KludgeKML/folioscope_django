779daad716eb55d44d621320ffddeb1f2528f82d
Keith M Lawrence <keith@kludge.co.uk>
2014-11-14 23:36:26 +0000

    use ENV vars for endpoints

--- folioscope/views.py
+++ folioscope/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+import os
 
 def index(request):
     context = {
-    	'document_endpoint': 'http://localhost:8000/docs',
-    	'sparql_endpoint': 'http://www.ancientwisdoms.ac.uk/sesame/repositories/saws',
-    	'url_base': 'http://www.ancientwisdoms.ac.uk/cts/urn:cts:',
+    	'document_endpoint': os.environ['DOCUMENT_ENDPOINT'],
+    	'sparql_endpoint': os.environ['SPARQL_ENDPOINT'],
+    	'url_base': os.environ['URL_BASE'],
     	}
     return render(request, 'folioscope/index.html', context)
