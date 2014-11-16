0a79b28a7240cfbfdece921d12a8666d17634666
Keith M Lawrence <keith@kludge.co.uk>
2014-11-16 18:10:54 +0000

    localise settings dialogs (and refactor them a little)

--- folioscope/views.py
+++ folioscope/views.py
@@ -5,6 +5,6 @@ def index(request):
     context = {
     	'document_endpoint': os.environ['DOCUMENT_ENDPOINT'],
     	'sparql_endpoint': os.environ['SPARQL_ENDPOINT'],
-    	'url_base': os.environ['URL_BASE'],
+    	'url_base': os.environ['URL_BASE']
     	}
     return render(request, 'folioscope/index.html', context)
